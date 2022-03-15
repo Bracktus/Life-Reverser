3acef9ab9476982d75fff6fd6ed0f2b1b5b9a354
Bracktus <specialistricky@gmail.com>
2022-03-15 09:52:47 +0000

    Fixed board printing, simplified constraints, handled board corners and added a function for board generation

--- linear-prog.py
+++ linear-prog.py
@@ -1,4 +1,5 @@
 from pulp import *
+import random
 
 def getNeighbours(r, c, width, height, choices):
     r = int(r)
@@ -49,32 +50,32 @@ def getNeighbours(r, c, width, height, choices):
     return acc
 
 def print_solution(choices):
-    for r in rows:
+    for c in cols:
         print()
-        for c in cols:
+        for r in rows:
             val = value(choices[r][c])
             s = " # " if (val == 1) else " . "
             print(s, end = "")
 
 def print_board(board):
-    for r in board:
+    for row in board:
         print()
-        for c in r:
-            s = " # " if (c == 1) else " . "
+        for item in row:
+            s = " # " if (item == 1) else " . "
             print(s, end = "")
 
-board = [[0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 1, 0, 0, 0, 0],
-         [0, 0, 0, 0, 1, 1, 0, 0],
-         [0, 0, 1, 1, 0, 0, 1, 1],
-         [0, 1, 0, 0, 0, 1, 0, 0],
-         [0, 1, 0, 1, 0, 1, 1, 0],
-         [0, 1, 1, 0, 1, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0]]
+def gen_rand_board(width, height):
+    acc = []
+    for h in range(height):
+        row = [random.randint(0,2) for i in range(width)]
+        acc.append(row)
+    return acc
+        
 
+width = 7
+height = 7
+board = gen_rand_board(width, height)
 
-width = len(board)
-height = len(board[0])
 
 rows = [str(i) for i in range(width)]
 cols = [str(i) for i in range(height)]
@@ -90,26 +91,33 @@ for r in rows:
     for c in cols:
         cell = choices[r][c]
         neighbours = getNeighbours(r,c, width, height, choices)
-        if board[int(r)][int(c)] == 1:
+        if board[int(c)][int(r)] == 1:
             #If it's alive, and it was previously alive, then it must've had 2 or 3 neighbours
             #If it's alive, and it was previously dead, then it must've had 3 neighbours
-            prob += lpSum(neighbours) >= 2 * cell + (1 - cell) * 3
+            prob += lpSum(neighbours) >= 3 - cell
             prob += lpSum(neighbours) <= 3
+
         else:
             #If it's dead, and was previously alive, then it must've had 0, 1 or 4+ neighbours
             #If it's dead, and was previously dead, then it must've had 0, 1, 2 or 4+ neighbours
-            or_var = or_variables[r][c] # 1 if max is 1 or 2, 
-                                        # 0 if max = 9
+            if len(neighbours) == 3:
+                #if you're on the corner
+                prob += lpSum(neighbours) >= 0
+                prob += lpSum(neighbours) <= 2 - cell
+            else:
+                or_var = or_variables[r][c] # 1 if max is 1 or 2, 
+                                            # 0 if max = 9
 
-            if_var = if_variables[r][c] # 1 if or_var implies cell
-                                        # 0 otherwise
-            
-            prob += if_var <= 1 - or_var + cell
-            prob += if_var >= 1 - or_var
-            prob += if_var >= cell
+                if_var = if_variables[r][c] # 1 if or_var implies cell
+                                            # 0 otherwise
+                
+                prob += if_var <= 1 - or_var + cell
+                prob += if_var >= 1 - or_var
+                prob += if_var >= cell
 
-            prob += lpSum(neighbours) >= 4 * (1 - or_var)
-            prob += lpSum(neighbours) <= 1 + (1 - if_var) + 8 * (1 - or_var)
+                prob += lpSum(neighbours) >= 4 * (1 - or_var)
+                prob += lpSum(neighbours) <= 1 + (1 - if_var) + (len(neighbours) - 1) * (1 - or_var) 
+
 
 prob.solve()
 
